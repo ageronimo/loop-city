4b073ff0b60347bcb2eef62383449de933b6c19d
ageronimo <angelageroniimo@gmail.com>
2016-11-30 08:59:03 -1000

    I FIXED THE HIP HOP ARRAY. i finished too but yay!

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Loops City
 
+
 // E1 - Mommy I can count
 var count;
 
@@ -14,6 +15,7 @@ for (count = 0; count <= 100; count++){
   }
 }
 
+
 // E2 - Noah's Arc
 var animalCount;
 
@@ -22,17 +24,34 @@ for (animalCount = 5; animalCount <10; animalCount++){
   console.log("There are " + animalCount + " animals in Noah's Arc.");
 }
 
+
 // E3 - Hip Hop Array
 var partyAnimals = ["Zebra", "Giraffe", "Lion", "Lemur", "Hippo"];
 var i;
-
+var sentence="";
 var partyActivity = [" drinks soda like there's no tomorrow.", " eats all the food in the fridge.", " tries on everyone's hats.", " leaps into the pool."];
-var a;
 
-for (i = 0; i < partyAnimals.length; i++){
-  console.log("The " + partyAnimals[i])
-  for (a = 0; a < partyActivity.length; a++){
-    console.log(partyActivity[a]);
+for (i = 0; i <= partyAnimals.length; i++){
+  console.log("The " + partyAnimals[i] + partyActivity[i]);
+}
+//^ fix later
+
+
+// E4 Merlin's Battle Weapon
+var weaponChest = ["Mace", "Sorcerer's Stone", "Excalibur"];
+var hero = {
+  wizard : "Merlin",
+  weapon : weaponChest[1],
+  power : 30
+};
+
+function wizardry(weapon){
+  if (weapon == "Sorcerer's Stone"){
+    console.log(hero.wizard + " shall slay the dragon with the " + weapon);
+  } else {
+    console.log(weapon + " has been equipped. " + hero.wizard + " is ready for war with the " + weapon + ".")
   }
 }
 
+wizardry(hero.weapon);
+// had trouble invoking bc i put wizardy :b
